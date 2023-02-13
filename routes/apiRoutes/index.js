c23c750c047529ce149e9a5dbda2db868189abc8
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-02-13 13:38:07 -0500

    Update index.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,18 +4,20 @@ const {createNewNote, updateDb} = require("../../lib/notes");
 const { v4: uuidv4 } = require('uuid');
 const {notes} = require("../../db/db.json");
 
-// show all notes in json data
+// show all in json data
 router.get("/notes", (req, res) => {
     let results = notes;
     res.json(results);
   });
 
+//Post
   router.post("/notes", (req, res) => {
     req.body.id = uuidv4();
     const newNote = createNewNote(req.body, notes);
     res.json(newNote);
   });  
 
+//Delete
   router.delete("/notes/:id" , (req, res) => {
     const params = req.params.id
     updateDb(params, notes);
